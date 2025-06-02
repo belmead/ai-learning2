72c7d0b4647d3dc5bff2d3d6ca74b5167d92d441
belmead <billrix@proton.me>
2025-06-02 09:49:12 -0500

    Create week1_basics.py

---
+++ week1/week1_basics.py
@@ -0,0 +1,36 @@
+## primitives
+
+# ints
+xInt = 1
+yInt = 2
+print(xInt, "is an int.")
+print(yInt, "is an int.")
+
+# floats
+xFloat = 1.234
+yFloat = 2.468
+print(xFloat, "is a float.")
+print(yFloat, "is a float.")
+
+# booleans
+xBool = True
+yBool = False
+print("The value of xBool is", xBool)
+print("The value of yBool is", yBool)
+
+## containers
+
+# lists
+xFruit = ["apple", "pear", "strawberry", "mango"]
+print(xFruit, " is a list.")
+print("The first item in the xFruit list is", xFruit[0], ".")
+print("The second through third items in the xFruit list are,", xFruit[1:3], ".")
+
+# dictionaries
+xDogs = {"GSD":"Rylee", "Daniel":"Doodle", "Scout":"Cavapoo"}
+print("The second dictionary item is", xDogs["Daniel"], ".") # can only directly point at values this way, not keys
+
+# sets
+
+
+# tuples
